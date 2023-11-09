a9cc135cb21cf860c568134a2617ac44f5357dc0
Fred <pf.robert974@gmail.com>
2023-11-10 10:15:15 +1100

    fix with int conversion via parseInt

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function addNumbers() {
-  currentResult = currentResult + userInput.value;
+  currentResult = currentResult + parseInt(userInput.value);
   outputResult(currentResult, "");
 }
 
