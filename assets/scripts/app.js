093dd4529a202ec2a9144ac31c8cadd8608229bb
Fred <pf.robert974@gmail.com>
2023-11-09 14:58:59 +1100

    test simple variable and constant

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,8 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = currentResult + 10;
+
+let calculationDescription = '(' + defaultResult + ' + 10)';
+
+outputResult(currentResult, calculationDescription);
