f9036f0bfcea5d890981afa42dd11c52d998a4a7
Fred <pf.robert974@gmail.com>
2023-11-10 10:11:03 +1100

    issue: create long string instead of add

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = currentResult + 10;
+function addNumbers() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, "");
+}
 
-let calculationDescription = `${defaultResult} + 10`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', addNumbers);
